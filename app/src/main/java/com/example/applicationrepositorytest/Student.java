0a48f72d289bdf5782d849baa8d30dc1e88c28d2
Antonio De Simone <antoniods311@gmail.com>
2016-09-08 12:31:16 +0200

    Added class Student

---
+++ app/src/main/java/com/example/applicationrepositorytest/Student.java
@@ -0,0 +1,18 @@
+package com.example.applicationrepositorytest;
+
+/**
+ * Created by antoniods311 on 08/09/16.
+ */
+public class Student {
+
+    private String name;
+    private String surname;
+    private double avg;
+
+    public Student(String name, String surname, double avg){
+        this.name = name;
+        this.surname = surname;
+        this.avg = avg;
+    }
+
+}
