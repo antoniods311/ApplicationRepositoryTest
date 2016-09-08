91ce164716f4bf645a4684a23dc4f57d720fc763
Antonio De Simone <antoniods311@gmail.com>
2016-09-08 13:07:40 +0200

    Modified field name in class Student. Added getter and setter in class Student

--- app/src/main/java/com/example/applicationrepositorytest/Student.java
+++ app/src/main/java/com/example/applicationrepositorytest/Student.java
@@ -7,12 +7,35 @@ public class Student {
 
     private String name;
     private String surname;
-    private double avg;
+    private double average;
 
-    public Student(String name, String surname, double avg){
+    public Student(String name, String surname, double average){
         this.name = name;
         this.surname = surname;
-        this.avg = avg;
+        this.average = average;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public double getAverage() {
+        return average;
+    }
+
+    public void setAverage(double average) {
+        this.average = average;
+    }
 }
