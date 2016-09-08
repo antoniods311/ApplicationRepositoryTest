8a858d5081924212853fa1c78d20b64ff3a1d677
Antonio De Simone <antoniods311@gmail.com>
2016-09-08 11:00:01 +0200

    Added getter and setter to class Enrico.java

--- app/src/main/java/com/example/applicationrepositorytest/Enrico.java
+++ app/src/main/java/com/example/applicationrepositorytest/Enrico.java
@@ -5,6 +5,21 @@ package com.example.applicationrepositorytest;
  */
 public class Enrico {
     private String a;
-
     private String b;
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
 }
